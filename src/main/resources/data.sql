2b5defaf7e25c508e090f3fe823f5cd544478d9c
student <student@FJ-1025>
2026-05-27 15:09:14 +0900

    2026/05/27

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,12 @@
-INSERT INTO users ( name, password)
+INSERT INTO users (name, password)
 VALUES
-( '田中太郎', 'test123'), 
-( '鈴木一郎', 'test456');
+('田中太郎', 'test123'), 
+('鈴木一郎', 'test456');
 
-INSERT INTO tasks (user_id, title, closing_date, progress, memo,time, date)
+INSERT INTO tasks (username, title, closing_date, progress, memo, time, date, priority, created_at, category_id)
 VALUES
-(1, '見積もり', '2025-12-31', 0, '案件に適した見積もりを取る' ,'40' , '2025-12-30');
+('田中太郎', '開発', '2026/03/26', '完了', '依頼された機能実装', 500, '2026/04/02', '中', '2026/03/26',2),
+('田中太郎', '絵', '2026/06/24', '未着手', '趣味の描画', 300, '2026/07/06', '中', '2026/06/22',1);
 
 INSERT INTO categories (category_id, category_name)
 VALUES
